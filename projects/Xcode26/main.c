31b9f55d3e7222c428a97143eece8d5a2eb4117c
Federico Gherardi <ghera@libero.it>
2026-01-10 18:03:51 +0100

    iOS: clang format Google style

--- projects/Xcode26/main.c
+++ projects/Xcode26/main.c
@@ -1,15 +1,15 @@
 /*******************************************************************************************
-*
-*   raylib [core] example - Input Gestures Detection
-*
-*   Example originally created with raylib 1.4, last time updated with raylib 4.2
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2016-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+ *
+ *   raylib [core] example - Input Gestures Detection
+ *
+ *   Example originally created with raylib 1.4, last time updated with raylib 4.2
+ *
+ *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+ *   BSD-like license that allows static linking with closed source software
+ *
+ *   Copyright (c) 2016-2024 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
 #ifndef RL_IOS_NO_EXAMPLE
 
 #include "raylib.h"
@@ -28,28 +28,29 @@ char gestureStrings[100][32];
 int currentGesture = GESTURE_NONE;
 int lastGesture = GESTURE_NONE;
 
-static void add_log_text(const char* title){
-    if (gesturesCount >= MAX_GESTURE_STRINGS)
-    {
-        for (int i = 0; i < MAX_GESTURE_STRINGS; i++) TextCopy(gestureStrings[i], "\0");
+static void add_log_text(const char* title) {
+    if (gesturesCount >= MAX_GESTURE_STRINGS) {
+        for (int i = 0; i < MAX_GESTURE_STRINGS; i++)
+            TextCopy(gestureStrings[i], "\0");
         gesturesCount = 0;
     }
     TextCopy(gestureStrings[gesturesCount], title);
     gesturesCount++;
 }
 
-void ios_ready(){
+void ready() {
     InitWindow(0, 0, "raylib [core] example - input gestures");
-    
+
     screenWidth = GetScreenWidth();
     screenHeight = GetScreenHeight();
-    
-    touchPosition = (Vector2){ 0, 0 };
-    touchArea = (Rectangle){ 220, 10, screenWidth - 230.0f, screenHeight - 20.0f };
-    
-    //SetGesturesEnabled(0b0000000000001001);   // Enable only some gestures to be detected
+
+    touchPosition = (Vector2){0, 0};
+    touchArea = (Rectangle){220, 10, screenWidth - 230.0f, screenHeight - 20.0f};
+
+    // SetGesturesEnabled(0b0000000000001001);   // Enable only some gestures to be detected
     SetTargetFPS(60);
     MAX_GESTURE_STRINGS = (screenHeight - 50) / 20;
+
 #if defined(PLATFORM_IOS)
     SafeAreaInsets insets = GetIOSSafeAreaInsets();
     add_log_text(TextFormat("SafeAreaInsets %.0f, %.0f, %.0f, %.0f",
@@ -57,13 +58,13 @@ void ios_ready(){
 #endif
 }
 
-void ios_update(bool viewSizeChanged)
-{
+void update(bool viewSizeChanged) {
     if (viewSizeChanged) {
         screenWidth = GetScreenWidth();
         screenHeight = GetScreenHeight();
-        touchArea = (Rectangle){ 220, 10, screenWidth - 230.0f, screenHeight - 20.0f };
+        touchArea = (Rectangle){220, 10, screenWidth - 230.0f, screenHeight - 20.0f};
         add_log_text(TextFormat("ViewSizeChanged %d x %d", GetMonitorWidth(0), GetMonitorHeight(0)));
+
 #if defined(PLATFORM_IOS)
         SafeAreaInsets insets = GetIOSSafeAreaInsets();
         add_log_text(TextFormat("SafeAreaInsets %.0f, %.0f, %.0f, %.0f",
@@ -75,33 +76,50 @@ void ios_update(bool viewSizeChanged)
     currentGesture = GetGestureDetected();
     touchPosition = GetTouchPosition(0);
 
-    if (IsMouseButtonPressed(0)) add_log_text("MouseButtonPressed");
-    if (IsMouseButtonReleased(0)) add_log_text("MouseButtonReleased");
+    if (IsMouseButtonPressed(0))
+        add_log_text("MouseButtonPressed");
+    if (IsMouseButtonReleased(0))
+        add_log_text("MouseButtonReleased");
 
-    if (CheckCollisionPointRec(touchPosition, touchArea) && (currentGesture != GESTURE_NONE))
-    {
-        if (currentGesture != lastGesture)
-        {
-            // Store gesture string
-            switch (currentGesture)
-            {
-                case GESTURE_TAP: add_log_text( "GESTURE TAP"); break;
-                case GESTURE_DOUBLETAP: add_log_text( "GESTURE DOUBLETAP"); break;
-                case GESTURE_HOLD: add_log_text( "GESTURE HOLD"); break;
-                case GESTURE_DRAG: add_log_text( "GESTURE DRAG"); break;
-                case GESTURE_SWIPE_RIGHT: add_log_text( "GESTURE SWIPE RIGHT"); break;
-                case GESTURE_SWIPE_LEFT: add_log_text( "GESTURE SWIPE LEFT"); break;
-                case GESTURE_SWIPE_UP: add_log_text( "GESTURE SWIPE UP"); break;
-                case GESTURE_SWIPE_DOWN: add_log_text( "GESTURE SWIPE DOWN"); break;
-                case GESTURE_PINCH_IN: add_log_text( "GESTURE PINCH IN"); break;
-                case GESTURE_PINCH_OUT: add_log_text( "GESTURE PINCH OUT"); break;
-                default: break;
+    if (CheckCollisionPointRec(touchPosition, touchArea) && (currentGesture != GESTURE_NONE)) {
+        if (currentGesture != lastGesture) {
+            switch (currentGesture) {
+                case GESTURE_TAP:
+                    add_log_text("GESTURE TAP");
+                    break;
+                case GESTURE_DOUBLETAP:
+                    add_log_text("GESTURE DOUBLETAP");
+                    break;
+                case GESTURE_HOLD:
+                    add_log_text("GESTURE HOLD");
+                    break;
+                case GESTURE_DRAG:
+                    add_log_text("GESTURE DRAG");
+                    break;
+                case GESTURE_SWIPE_RIGHT:
+                    add_log_text("GESTURE SWIPE RIGHT");
+                    break;
+                case GESTURE_SWIPE_LEFT:
+                    add_log_text("GESTURE SWIPE LEFT");
+                    break;
+                case GESTURE_SWIPE_UP:
+                    add_log_text("GESTURE SWIPE UP");
+                    break;
+                case GESTURE_SWIPE_DOWN:
+                    add_log_text("GESTURE SWIPE DOWN");
+                    break;
+                case GESTURE_PINCH_IN:
+                    add_log_text("GESTURE PINCH IN");
+                    break;
+                case GESTURE_PINCH_OUT:
+                    add_log_text("GESTURE PINCH OUT");
+                    break;
+                default:
+                    break;
             }
         }
     }
 
-    // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
@@ -111,34 +129,56 @@ void ios_update(bool viewSizeChanged)
 
     DrawText("GESTURES TEST AREA", screenWidth - 270, screenHeight - 40, 20, Fade(GRAY, 0.5f));
 
-    for (int i = 0; i < gesturesCount; i++)
-    {
-        if (i % 2 == 0){
-            DrawRectangle(10, 30 + 20*i, 200, 20, Fade(LIGHTGRAY, 0.5f));
-        }else{
-            DrawRectangle(10, 30 + 20*i, 200, 20, Fade(LIGHTGRAY, 0.3f));
+    for (int i = 0; i < gesturesCount; i++) {
+        if (i % 2 == 0) {
+            DrawRectangle(10, 30 + 20 * i, 200, 20, Fade(LIGHTGRAY, 0.5f));
+        } else {
+            DrawRectangle(10, 30 + 20 * i, 200, 20, Fade(LIGHTGRAY, 0.3f));
         }
-        if (i < gesturesCount - 1){
-            DrawText(gestureStrings[i], 35, 36 + 20*i, 10, DARKGRAY);
-        }else{
-            DrawText(gestureStrings[i], 35, 36 + 20*i, 10, MAROON);
+        if (i < gesturesCount - 1) {
+            DrawText(gestureStrings[i], 35, 36 + 20 * i, 10, DARKGRAY);
+        } else {
+            DrawText(gestureStrings[i], 35, 36 + 20 * i, 10, MAROON);
         }
     }
 
     DrawRectangleLines(10, 29, 200, screenHeight - 50, GRAY);
-    DrawText(
-             TextFormat("TOUCH COUNT: %d", GetTouchPointCount()),
-             50, 15, 10, GRAY
-             );
+    DrawText(TextFormat("TOUCH COUNT: %d", GetTouchPointCount()), 50, 15, 10, GRAY);
 
-    for(int i=0; i < GetTouchPointCount(); i++){
+    for (int i = 0; i < GetTouchPointCount(); i++) {
         DrawCircleV(GetTouchPosition(i), 30, MAROON);
     }
+
     EndDrawing();
 }
 
-void ios_destroy(){
-    CloseWindow();        // Close window and OpenGL context
+void destroy() {
+    CloseWindow();
 }
+
+#if defined(PLATFORM_IOS)
+void ios_ready() {
+    ready();
+}
+
+void ios_update(bool viewSizeChanged) {
+    update(viewSizeChanged);
+}
+
+void ios_destroy() {
+    destroy();
+}
+#else
+int main() {
+    ready();
+
+    while (!WindowShouldClose()) {
+        update(false);
+    }
+
+    destroy();
+    return 0;
+}
+#endif
 
 #endif
