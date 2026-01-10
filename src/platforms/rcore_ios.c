c0b746097ddc594235f1a8df837c7614ab610c11
Federico Gherardi <ghera@libero.it>
2026-01-10 17:47:16 +0100

    iOS: fix FLAG_WINDOW_UNFOCUSED management

--- src/platforms/rcore_ios.c
+++ src/platforms/rcore_ios.c
@@ -800,11 +800,11 @@ static void SendGestureEvent(NSSet<UITouch *> * touches, int action)
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    CORE.Window.flags &= ~FLAG_WINDOW_UNFOCUSED;
+    CORE.Window.flags |= FLAG_WINDOW_UNFOCUSED;
 }
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    CORE.Window.flags |= FLAG_WINDOW_UNFOCUSED;
+    CORE.Window.flags &= ~FLAG_WINDOW_UNFOCUSED;
 }
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
